18a061eb235eaec985485028f55a147a01112364
vincent.lepleux@gmail.com <vincent.lepleux@gmail.com@ed733291-e447-d82b-ecf0-d89d13285a68>
2012-09-24 14:42:28 +0000

    Continuation du premier écran

--- trunk/easy_parking/src/com/easy_parking/HomeActivity.java
+++ trunk/easy_parking/src/com/easy_parking/HomeActivity.java
@@ -3,6 +3,9 @@ package com.easy_parking;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends Activity {
 
@@ -10,11 +13,16 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button btnByAdress = (Button) findViewById(R.id.buttonByAdress);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
+    }
+    
+    public void showAdressBlock(View view) {
+        ((LinearLayout) findViewById(R.id.AdressBlock)).setVisibility(0);
     }
 }
