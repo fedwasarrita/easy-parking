d14a385a69f4bd7b0f8b153626c699a6d965ef80
vincent.lepleux <vincent.lepleux@gmail.com>
2012-10-19 09:25:50 +0000

    Modification Provider et modification home activity pour la gestion du formulaire

--- easy_parking/src/com/business/PlaceBusiness.java
+++ easy_parking/src/com/business/PlaceBusiness.java
@@ -14,15 +14,31 @@ import android.location.LocationManager;
 import com.easy_parking.R;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.OverlayItem;
+import com.model.AO.EasyException;
 import com.model.AO.SearchForm;
 import com.model.AO.SitesOverlay;
+import com.model.DTO.Place;
+import com.provider.PlaceProvider;
+
 import android.location.Location;
 import android.os.Bundle;
 
 public class PlaceBusiness {
 
-	public void GetPlacesByPosition(SearchForm form){
-			//TODO call the Data Provider
-			
+	@SuppressWarnings("finally")
+	public List<Place> GetPlacesByPosition(SearchForm form){
+			List<Place> list = null;
+			try 
+			{
+				
+				list= PlaceProvider.GetAllPlaces(form);
+			} 
+			catch (EasyException e) {
+				
+			}
+			finally
+			{
+				return list;
+			}
 	}
 }
