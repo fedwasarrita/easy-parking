fb226d4e3be6eca475635cab760776635b1c80bc
carpentier.au <carpentier.au@gmail.com>
2012-11-16 10:16:01 +0000

    maj path webservice

--- easy_parking/src/com/utils/Paths.java
+++ easy_parking/src/com/utils/Paths.java
@@ -3,6 +3,6 @@ package com.utils;
 public class Paths {
 	
 	// URL racine du serveur WS
-	public static final String ROOT_URL = "http://monserveur.com";
+	public static final String ROOT_URL = "http://wwww.cours-paes-bob.com/Android/index.php";
 
 }
