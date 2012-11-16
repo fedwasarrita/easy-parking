4e03643ceed02106bd1cc8c3e4827d41988bcdf3
crazyilko <crazyilko@ed733291-e447-d82b-ecf0-d89d13285a68>
2012-11-16 08:20:13 +0000

    Fix network management !!

--- trunk/easy_parking/src/com/easy_parking/SplashScreenActivity.java
+++ trunk/easy_parking/src/com/easy_parking/SplashScreenActivity.java
@@ -4,28 +4,25 @@ package com.easy_parking;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Bundle;
-import android.provider.Settings;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 
 public class SplashScreenActivity extends Activity {
 
 	// TimeOut in milliseconds
-	private final int splashDelay = 5*1000;
+	private final int splashDelay = 3*1000;
 	private Timer timeOutTimer;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
+		
+		
+		
 		
 		
 		TimerTask task = new TimerTask() {
@@ -45,11 +42,7 @@ public class SplashScreenActivity extends Activity {
 		this.timeOutTimer = new Timer();
 		timeOutTimer.schedule(task, splashDelay);
 		
-		if(!checkDeviceConnectivity()){
-			//task.cancel();
-			//this.timeOutTimer.cancel();
-			showConnectionStatus();
-		}
+		
 	
 	}
 
@@ -59,52 +52,5 @@ public class SplashScreenActivity extends Activity {
 		return true;
 	}
 
-	/*
-	 * Check Internet connection of the device. Only Mobile broadband and wifi
-	 * connection are tested. The WiMax and bluetooth are not checked
-	 */
-	private boolean checkDeviceConnectivity() {
-		ConnectivityManager cm = (ConnectivityManager) ((Context) SplashScreenActivity.this).getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-		if(activeNetwork==null)
-			return false;
-		if (!activeNetwork.isConnected()) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	/* Build the Network status pop-up */
-	private void showConnectionStatus(){
-		AlertDialog.Builder builder= new AlertDialog.Builder(this);
-		builder.setCancelable(true);
-		builder.setTitle("Network status");
-		builder.setMessage("You are not connected to internet");
-		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-				
-			}
-		});
-		
-		builder.setNegativeButton("Go to network settings", new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// Open android network settings
-				Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
-				//intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				//intent.setClassName("com.android.settings", "com.android.phone.NetworkSetting");
-				startActivity(intent);
-				
-			}
-		});
-		//Create and show the AlertDialog
-		AlertDialog alert=builder.create();
-		alert.show();	
-	}
 	
 }
