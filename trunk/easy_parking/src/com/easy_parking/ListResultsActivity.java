ed06f0e26966d3a86391b34a7645bf7a787c8377
vincent.lepleux@gmail.com <vincent.lepleux@gmail.com@ed733291-e447-d82b-ecf0-d89d13285a68>
2012-11-23 09:50:47 +0000

    fix bug sur liste (vue)

--- trunk/easy_parking/src/com/easy_parking/ListResultsActivity.java
+++ trunk/easy_parking/src/com/easy_parking/ListResultsActivity.java
@@ -25,38 +25,11 @@ public class ListResultsActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_results);
-        Place p1 = new Place();
-        p1.setGratuite(false);
-        p1.setAdresse(new Adresse());
-        p1.getAdresse().setAdresse("31 rue de la Digue");
-        p1.setSecurisee(true);
-        p1.setDistance(99);
-        Place p2 = new Place();
-        p2.setGratuite(true);
-        p2.setAdresse(new Adresse());
-        p2.getAdresse().setAdresse("402 Boulevard Victore Hugo");
-        p2.setSecurisee(false);
-        p2.setHandicapee(true);
-        p2.setDistance(908);
-        Place p3 = new Place();
-        p3.setGratuite(false);
-        p3.setAdresse(new Adresse());
-        p3.getAdresse().setAdresse("99 Boulevard Vauban");
-        p3.setSecurisee(false);
-        p3.setHandicapee(false);
-        p3.setDistance(190);
-        Place p4 = new Place();
-        p4.setGratuite(true);
-        p4.setAdresse(new Adresse());
-        p4.getAdresse().setAdresse("73 Rue nationale");
-        p4.setSecurisee(false);
-        p4.setHandicapee(true);
-        p4.setDistance(45);
-        m_places.add(p1);
-        m_places.add(p2);
-        m_places.add(p3);
-        m_places.add(p4);
-        setListAdapter(new PlaceAdapter(this, R.layout.row, (ArrayList<Place>) PlaceProvider.listPlaces));
+        if(PlaceProvider.listPlaces != null)
+        {
+        	setListAdapter(new PlaceAdapter(this, R.layout.row, (ArrayList<Place>) PlaceProvider.listPlaces));
+        }
+        
         
         ListView list = this.getListView();
         list.setOnItemClickListener(new OnItemClickListener() {
