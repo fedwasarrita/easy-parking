db85bec9fc48a5944aa77cb1a9efea522b36adf4
crazyilko <crazyilko@ed733291-e447-d82b-ecf0-d89d13285a68>
2012-12-07 10:37:17 +0000

    Add TabHost Listeners

--- easy_parking/src/com/easy_parking/TabResultsActivity.java
+++ easy_parking/src/com/easy_parking/TabResultsActivity.java
@@ -4,27 +4,34 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TabHost.TabSpec;
 
 import com.model.AO.SearchForm;
 
 public class TabResultsActivity extends TabActivity {
 
+	private TabHost tabHost;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_results);
         
-        TabHost tabHost = getTabHost();
+        this.tabHost = getTabHost();
         
         // Tab for MapView
         TabSpec mapSpec = tabHost.newTabSpec("Carte");
+        
         // setting Title and Icon for the Tab
         Intent tempIntent=this.getIntent();
         SearchForm searchF= (SearchForm)tempIntent.getSerializableExtra("SearchForm");
         
         mapSpec.setIndicator("Carte", getResources().getDrawable(R.drawable.ic_action_search_light));
+        
         Intent mapIntent = new Intent(this, MapViewActivity.class);
         mapIntent.putExtra("LocationWay", getIntent().getStringExtra("LocationWay"));
         mapIntent.putExtra("SearchForm", searchF);
@@ -43,8 +50,53 @@ public class TabResultsActivity extends TabActivity {
         tabHost.addTab(mapSpec); // Adding map tab
         tabHost.addTab(listSpec); // Adding list tab
         
+        //Add listeners on tab buttons
+       
+        
+        
+        this.tabHost.setOnTabChangedListener(new OnTabChangeListener() {
+			@Override
+			public void onTabChanged(String tabId) {
+				if(tabId.equals("Carte")){
+					getTabWidget().getChildTabViewAt(0).getResources().getDrawable(R.drawable.ic_action_search_light);
+					getTabWidget().getChildTabViewAt(1).getResources().getDrawable(R.drawable.ic_view_as_list_dark);
+					
+				}else{
+					getTabWidget().getChildTabViewAt(0).getResources().getDrawable(R.drawable.ic_action_search_dark);
+					getTabWidget().getChildTabViewAt(1).getResources().getDrawable(R.drawable.ic_view_as_list_light);
+				}
+			}
+			
+		});
+        
+        
+        
+        
+        
+        
+     /*   tabHost.getTabWidget().getChildAt(0).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				getTabWidget().getChildTabViewAt(0).getResources().getDrawable(R.drawable.ic_action_search_light);
+				getTabWidget().getChildTabViewAt(1).getResources().getDrawable(R.drawable.ic_view_as_list_dark);
+				
+			}
+		});
+        
+        tabHost.getTabWidget().getChildAt(1).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				getTabWidget().getChildTabViewAt(0).getResources().getDrawable(R.drawable.ic_action_search_dark);
+				getTabWidget().getChildTabViewAt(1).getResources().getDrawable(R.drawable.ic_view_as_list_light);
+				
+			}
+		});
+		
+      */  
     }
-
+	
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_tab_results, menu);
